49a9c9a0e36957f332155479176eadf315e001a8
adyyogap07 <136486556+adyyogap07@users.noreply.github.com>
2023-07-05 18:19:01 +0700

    Update Projek UAS.cpp

--- Projek UAS.cpp
+++ Projek UAS.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iomanip> // Untuk menggunakan fungsi setprecision()
+#include <iomanip> // Untuk menggunakan fungsi setprecision
 
 using namespace std;
 
